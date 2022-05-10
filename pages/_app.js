e6ff784c6c9327cbdcfdef2ad283a2ef4bddfb46
Jacob Ford <j@cobford.com>
2022-05-10 18:59:21 -0400

    Add Fathom

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import * as Fathom from 'fathom-client'
+
+const fathomTrackingCode = process.env.FATHOM_SITEID
+
+function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load(fathomTrackingCode, {
+      url: 'https://sub.moon.sex/script.js'
+    })
+  })
+
+  return <Component {...pageProps} />
+}
+
+export default App
