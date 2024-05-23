dbe2694de34933a55c888d9f403c6f919d8ece9c
Jacob Ford <j@cobford.com>
2024-05-23 00:54:36 -0400

    Remove boilerplate comments

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import Fathom from './Fathom'
 
 export default function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
   children,
 }: {
   children: React.ReactNode
