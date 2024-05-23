879907e3e553ce59fdeed8de16f168eb1fa217d2
Jacob Ford <j@cobford.com>
2024-05-23 00:39:20 -0400

    Copy default app/layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <style>{styles}</style>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
