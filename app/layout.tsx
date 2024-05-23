5dea0d74d6854b32446a85e9c35ff71d646b11dc
Jacob Ford <j@cobford.com>
2024-05-23 01:27:27 -0400

    Fix hydration error & add Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import Fathom from './Fathom'
 import './global.css'
 
+import { Metadata } from 'next'
+export const metadata: Metadata = {
+  title: 'The Moon',
+  description: 'Waxing or waning live in your browser',
+}
+
 export default function RootLayout({
   children,
 }: {
--- app/page.js
+++ app/page.js
@@ -2,16 +2,8 @@ import Moon from '../api/moon-phase'
 
 export default function Page() {
   return (
-    <>
-      <head>
-        <title>The Moon</title>
-        <meta charSet="utf-8" />
-      </head>
-      <body>
-        <div className="flex-suspender">
-          <Moon/>
-        </div>
-      </body>
-    </>
+    <div className="flex-suspender">
+      <Moon/>
+    </div>
   )
 }
