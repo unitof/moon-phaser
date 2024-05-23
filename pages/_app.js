69c5e0b4b1a7ce79c9570d0f2315e99f3db497d9
Jacob Ford <j@cobford.com>
2024-05-22 23:44:24 -0400

    Remove Fathom custom domain

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,13 @@
 import { useEffect } from 'react';
 import * as Fathom from 'fathom-client'
 
-const fathomTrackingCode = process.env.NEXT_PUBLIC_FATHOM_SITEID
+const fathomSideId = process.env.NEXT_PUBLIC_FATHOM_SITEID
 
 function App({ Component, pageProps }) {
 
   useEffect(() => {
     // Initialize Fathom when the app loads
-    Fathom.load(fathomTrackingCode, {
-      url: 'https://sub.moon.sex/script.js'
-    })
+    Fathom.load(fathomSideId)
   })
 
   return <Component {...pageProps} />
